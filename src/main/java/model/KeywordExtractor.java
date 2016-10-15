5bc7e7cbf12946f2e8bcb24de75b57790848f964
Joakim <joakim.0565@gmail.com>
2016-10-15 17:40:34 -0400

    Add start of parsing string in OpenNLP, encounters error

--- src/main/java/model/KeywordExtractor.java
+++ src/main/java/model/KeywordExtractor.java
@@ -1,5 +1,16 @@
 package model;
 
+import java.io.BufferedInputStream;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+
+import opennlp.tools.parser.Parse;
+import opennlp.tools.parser.Parser;
+import opennlp.tools.parser.ParserFactory;
+import opennlp.tools.parser.ParserModel;
+
 public class KeywordExtractor {
 	
 	public KeywordExtractor() {
@@ -7,8 +18,42 @@ public class KeywordExtractor {
 	}
 	
 	public String[] extractKeywords(String str) {
+		try {
+			parseString(str);
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		return null;
 	}
 	
+	private String parseString(String str) throws FileNotFoundException {
+			InputStream modelIn = new FileInputStream("util/en-parser-chunking.bin");
+			Parser parser;
+		    try {
+		      ParserModel model = new ParserModel(modelIn);
+		      parser = ParserFactory.create(model);
+		      Parse p = new Parse(str, null, null, 0, 0);
+		      p = parser.parse(p);
+		      System.out.println(p.getText());
+		    }
+		    catch (IOException e) {
+		      e.printStackTrace();
+		    }
+		    finally {
+		      if (modelIn != null) {
+		        try {
+		          modelIn.close();
+		        }
+		        catch (IOException e) {
+		        }
+		      }
+		    }
+
+		    		
+
+
+		return null;
+	}
 
 }
